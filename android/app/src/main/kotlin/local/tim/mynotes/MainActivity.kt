68555f6d2a4ddbd17aae3cf07d53da9864786946
Tim Ahn <tim.ahn@outlook.com>
2024-03-24 23:14:04 +0500

    initial commit

---
+++ android/app/src/main/kotlin/local/tim/mynotes/MainActivity.kt
@@ -0,0 +1,5 @@
+package local.tim.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home '),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                print(FirebaseAuth.instance.currentUser);
+                return const Text('Done');
+              default:
+                return const Text('Loading...');
+            }
+          }),
+    );
+  }
+}
